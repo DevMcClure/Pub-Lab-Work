1931ac26b0ec791e33c720676ea565fe573b0edc
Dev Work <DevWork@Devans-MacBook-Pro.local>
2022-05-11 13:24:16 +0100

    second commit

--- src/pub.py
+++ src/pub.py
@@ -1,8 +1,6 @@
 class Pub:
     def __init__(self, name, till):
         self.name = name
-        self.till = till 
-
-
-    def increase_till(self, amount):
-        self.till += amount   
+        self.till = till
+        self.drinks = []
+        
