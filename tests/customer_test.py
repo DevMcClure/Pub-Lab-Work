ef5037186a9eab48affd4c5cf0dd939ecf5e55f3
Dev Work <DevWork@Devans-MacBook-Pro.local>
2022-05-11 13:55:19 +0100

    customer test function done

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -1,4 +1,12 @@
 import unittest 
-from src.pub import Pub
+from src.customer import Customer
 
 class TestCustomer(unittest.TestCase):
+
+    def setUp(self):
+        self.customer1 = Customer("Bob", 20.00)
+        self.customer2 = Customer("Dev", 15.00)
+
+
+    def test_customer_name(self):
+        self.assertEqual("Bob", self.customer1.name)   
