dd26af28715d41c0e36b3f48ea4aadfd7689b866
Brian Kerr <briantkerr91@gmail.com>
2022-05-11 13:49:00 +0100

    passing back to dev

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -2,3 +2,11 @@ import unittest
 from src.drink import Drink
 
 class TestDrink(unittest.TestCase):
+
+    def setUp(self):
+        self.drink1 = Drink("Dark and Stormy", 4.50)
+        self.drink2 = Drink("Zombie", 6.25)
+    
+    def test_drink_name(self):
+        self.assertEqual("Zombie", self.drink2.name)
+        
